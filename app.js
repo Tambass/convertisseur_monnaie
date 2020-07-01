5992da423dad51bd20f570f50558b6d923b70317
Tambass <tambass85@gmail.com>
2020-07-01 10:10:19 +0200

    ajout function recupererValeur

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-var url = "http://data.fixer.io/api/latest?access_key=b26a227339883b25230eb2af24fdde14&base=EUR&symbols=GBP,JPY,EUR,USD"
+var url =
+  "http://data.fixer.io/api/latest?access_key=b26a227339883b25230eb2af24fdde14&base=EUR&symbols=GBP,JPY,EUR,USD";
 
-
+function recupererValeur() {
+  $.ajax({
+    url: url,
+    type: "GET",
+    success: function (res, status, req) { // Callback
+      console.log("res: ", res.rates.USD);
+    },
+  });
+}
